0df32df668d04cdefbc3aa87db936aaccf8bbcb9
kumawabh <kumawabh@WL350183>
2020-07-12 21:54:45 +0530

    added scss file support

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 
+import './main.scss';
+
 export class Main extends Component<{}, {}> {
     render() {
         return (
-            <div>
-                This is react typescipt app.
+            <div className="main">
+                <div>
+                    This is react typescipt app.
+                </div>
             </div>
         );
     }
