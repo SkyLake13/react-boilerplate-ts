44bbfbfbff1c640b48e365c39bcd5c0eceee11a3
kumawabh <kumawabh@WL350183>
2021-03-10 15:18:08 +0530

    Added portal

--- src/app/header/Header.tsx
+++ src/app/header/Header.tsx
@@ -3,9 +3,39 @@ import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 
 import './Header.scss';
+import { Portal } from "../Portal";
+import { UserCard } from "./user-card/UserCard";
 
 export function Header(props: { onNavClick: Function }): JSX.Element {
     const [variant] = useState<'dark' | 'light' | undefined>('dark');
+    const [showUserCard, setShowUserCard] = useState<boolean>(false);
+    const [clickPosition, setClickPosition] = useState<{x: number, y: number}>({x: 0, y: 0});
+
+    const userCard = () => {
+        if(showUserCard) {
+            let container = document.getElementById('portal');
+            if(!container) {
+                container = document.createElement('div');
+                document.appendChild(container);
+            }
+            container.style.left = `${clickPosition.x}px`;
+            container.style.top = `${clickPosition.y}px`;
+            container.style.position = 'absolute';
+            
+
+            return <Portal container={container}>
+                        <UserCard userName="skylake" fullName="Abhishe Kumar"/>
+                    </Portal>
+        }
+
+        return <></>;
+    }
+
+    const userClick = (event: any) => {
+        setShowUserCard(!showUserCard);
+        setClickPosition({ x: event.clientX, y: event.clientY });
+        console.log(!showUserCard);
+    }
 
     return (
         <Navbar bg={variant} variant={variant}>
@@ -17,9 +47,10 @@ export function Header(props: { onNavClick: Function }): JSX.Element {
                     Rainbow
                 </div>
             </Navbar.Brand>
-            <div className="user-round">
+            <div className="user-round" onClick={(event) => userClick(event)}>
                 img
             </div>
+            {userCard()}
         </Navbar>
     );
 }
---
+++ src/app/header/user-card/UserCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './UserCard.scss';
+
+
+export function UserCard(props: { userName: string, fullName: string }): JSX.Element {
+    return (
+        <div className="user-card">
+            <div>
+                {props.userName}
+            </div>
+            <div>
+                {props.fullName}
+            </div>
+        </div>
+    );
+}
