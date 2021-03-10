2b1778a485a77baee78894ad633c6a0ba7e6a6cd
kumawabh <kumawabh@WL350183>
2021-03-10 15:32:08 +0530

    Added Router component but not using it yet

---
+++ src/app/AppRouter.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+  } from 'react-router-dom';
+
+export function AppRouter(): JSX.Element {
+    return (
+        <Router>
+            <Switch>
+                <Route path="">
+
+                </Route>
+                <Route path="/">
+
+                </Route>
+            </Switch>
+        </Router>
+    );
+}
