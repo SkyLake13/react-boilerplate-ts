092b8c6140e13bdbe34bc7d1bc34b8ac20f985bc
kumawabh <kumawabh@WL350183>
2021-03-13 13:19:08 +0530

    Removed unused code

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -1,26 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
     Switch,
     Route
 } from 'react-router-dom';
-import User from './pages/user/User';
-import { NavItem } from './side-nav/SideNav';
-import { getPages } from './App';
+
 import { DyanmicPage } from './pages/dyanmic-page/DynamicPage';
 
 export function AppRoutes(): JSX.Element {
-    const [, setNav] = useState<NavItem[]>([])
-
-    useEffect(() => {
-        getPages().then(res => setNav(res as NavItem[]));
-    }, []);
-
-    
     return (
         <Switch>
-            <Route path="/user">
-                <User />
-            </Route>
             <Route path="/:page">
                 <DyanmicPage />
             </Route>
--- src/app/pages/user/User.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function User(): JSX.Element {
-    return (
-        <div>
-            This is user page
-        </div>
-    );
-}
