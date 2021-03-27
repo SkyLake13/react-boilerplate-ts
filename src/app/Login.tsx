977ff4ead82289231e4b8a38f8a9eedfb25dfee6
kumawabh <kumawabh@WL350183>
2021-03-28 00:55:28 +0530

    added basic redirect logic after login

--- src/app/Login.tsx
+++ src/app/Login.tsx
@@ -1,7 +1,7 @@
 import { TextField, Container, Button, Typography, Paper } from '@material-ui/core';
 import { CSSProperties } from 'react';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import './App.scss';
 import image from "../assets/react.png";
@@ -17,11 +17,18 @@ const form: CSSProperties = {
     justifyItems: 'center'
 }
 
+function useQuery(): URLSearchParams {
+    return new URLSearchParams(useLocation().search);
+  }
+
 export default function Login(): JSX.Element {
+    const queryParams = useQuery();
+
     const { register, handleSubmit, errors } = useForm<FormData>();
 
     const onSubmit = (dta: FormData) => {
         console.log(dta);
+        window.location.href = queryParams.get('return_url') ?? window.location.href;
     };
 
     return (
