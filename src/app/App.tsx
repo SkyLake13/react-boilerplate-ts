e0922268c074180f2816cb943018b9cade8823c3
kumawabh <kumawabh@WL350183>
2021-03-27 18:26:33 +0530

    working form

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,38 @@
-import './App.scss';
-import image from '../assets/react.png'
+import { TextField, Container, Button } from '@material-ui/core';
+import { CSSProperties } from 'react';
+import { useForm } from 'react-hook-form';
 
-export default function() {
+import './App.scss';
+
+interface FormData {
+    username: string;
+    password: string;
+}
+
+const form: CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyItems: 'center'
+}
+
+export default function () {
+    const { register, handleSubmit, errors } = useForm<FormData>();
+
+    const onSubmit = (dta: FormData) => {
+        console.log(dta);
+    };
+
     return (
-        <div className="main">
-                This is react typescipt app.
-                <img src={image}></img>
-        </div>
+        <Container maxWidth="xs">
+            <form style={form} onSubmit={handleSubmit(onSubmit)}>
+                <TextField label="Username" variant="outlined" style={{margin: '10px'}}
+                    name="username" {...(errors.username && {error: true})} inputRef={register({ required: true })} />
+                <TextField type="password" label="Password" variant="outlined" style={{margin: '10px'}}
+                    name="password" {...(errors.password && {error: true})} inputRef={register({ required: true })} />
+                <Button type="submit" variant="contained" color="primary" style={{margin: '10px'}}>
+                    Primary
+                </Button>
+            </form>
+        </Container>
     );
 }
